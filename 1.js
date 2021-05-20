27887ba025a12f154d8466e1a3be17e6e1f0ec18
m-shahrestani <9728054@ceit.local>
2021-05-21 02:12:47 +0430

    Add files

---
+++ 1.js
@@ -0,0 +1,64 @@
+﻿<html>
+	<head>
+		<style>
+		.A{background:url("toop1.jpg");}
+		</style>
+
+		<script language="javascript">
+		function table(p,q)
+		{
+		var i,j;
+		document.write("<table border='1' width='80%' height='80%'>");
+			for(i=0;i<=(p-1);i++)
+			{
+				document.write("<tr>");
+				for(j=1;j<=q;j++)
+				{
+					if(i==0&j==1)
+					document.write("<td id='(i*p)+j)' class='A'>"+((i*p)+j)+"</td>");
+					else
+					document.write("<td id='(i*p)+j)' class=''>"+((i*p)+j)+"</td>");
+				}
+				document.write("</tr>");
+			}
+			document.write("</table>");
+		}
+
+		function bala(o)
+		{
+
+			document.getElementById(o).style.background="url('toop1.jpg')";
+			document.getElementById(o+5).style.background="url()";
+		}
+		function payin(o)
+		{
+			
+			document.getElementById(o).style.background="url('toop1.jpg')";
+			document.getElementById(o-5).style.background="url()";
+		}
+		function rast(o)
+		{
+			
+			document.getElementById(o).style.background="url('toop1.jpg')";
+			document.getElementById(o-1).style.background="url()";
+		}
+		function chap(o)
+		{
+			document.getElementById(o).style.background="url('toop1.jpg')";
+			document.getElementById(o+1).style.background="url()";
+		}
+		</script>
+
+	</head>
+	<body>
+		<script language="javascript">
+		table(5,5);
+		var o=1;
+		document.write('<input type="button" value="↑" onclick="bala(o-5); ">');
+		document.write('<input type="button" value="↓" onclick="payin(o+5); ">');
+		document.write('<input type="button" value="→" onclick="rast(o+1); ">');
+		document.write('<input type="button" value="←" onclick="chap(o-1); ">');
+
+		</script>		
+	</body>
+</html>
---
+++ main.js
@@ -0,0 +1,44 @@
+﻿<html>
+<head>
+<style>
+.mat{
+background-image:url('http://s2.freeupload.ir/i/00055/2c063w6bhgzf.jpg');
+}
+</style>
+<script>
+	function table(m,n)
+	{
+		var i,j;
+		document.write("<table border='1' height='50%' width='35%'>");
+		for(i=1;i<=m;i++)
+		{
+			document.write("<tr>");
+			for(j=1;j<=n;j++){
+				if(i==1 && j==1)
+					document.write("<td id='"+(i*9+j-9)+"' class='mat'> </td>");
+				else
+					document.write("<td id='"+(i*9+j-9)+"' class=''> </td>");	}			
+			document.write("</tr>");
+		}
+		document.write("</table>");
+	}
+	
+	function move(n,d)
+	{
+	document.getElementById(n+d).style.background="url('a.jpg')";
+	document.getElementById(n).style.background="url()";
+	}
+</script>
+</head>
+
+<body>
+<script language="javascript">
+	table(9,9);
+var n=1;
+document.write('<input type="button" value="↑" onclick="move(n,-9); n-=9;"><br>');
+document.write('<input type="button" value="←" onclick="move(n,-1); n--;">');
+document.write('<input type="button" value="→" onclick="move(n,+1); n++;"><br>');
+document.write('<input type="button" value="↓" onclick="move(n,+9); n+=9;">');
+</script>
+</body>
+</html>
